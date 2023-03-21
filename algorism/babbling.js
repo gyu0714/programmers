a307874b4aa1f0f833828dc72da4e211c868e56d
gyu0714 <lagyu99@gmail.com>
2023-03-21 14:04:55 +0900

    옹알이 문제 테스트 케이스 추가

--- algorism/babbling.js
+++ algorism/babbling.js
@@ -1,7 +1,11 @@
 const solution = (babbling) => {
-  return babbling
+  const result = babbling
     .map((_) => _.replace(/aya|ye|woo|ma/g, ''))
-    .filter((e) => (e = !e)).length;
+    .filter((e) => !e).length;
+
+  console.log(result);
+  return result;
 };
 
-solution([['aya', 'yee', 'u', 'maa', 'wyeoo']]);
+solution(['aya', 'yee', 'u', 'maa', 'wyeoo']);
+solution(['aya', 'yee', 'u', 'ma', 'wooyeoo']);
